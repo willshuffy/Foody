11a36b0967ce2c34bf0711c92a82b295e4089a35
willshuffy <saputrawilly26@gmail.com>
2021-04-06 16:05:24 +0700

    Build SplashScreen

--- app/src/main/java/com/umestudio/foody/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/umestudio/foody/ui/splashscreen/SplashActivity.kt
@@ -1,19 +1,28 @@
-package com.umestudio.foody
+package com.umestudio.foody.ui.splashscreen
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.view.WindowManager
+import com.umestudio.foody.MainActivity
+import com.umestudio.foody.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         initStatusBar()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },3000)
     }
 
 
@@ -28,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         }
         if (Build.VERSION.SDK_INT >= 19) {
             window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
